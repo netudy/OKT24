b0d78c4e80d00db6ba1cef5c2f588d45316aba2a
Abbe <abdmolight@gmail.com>
2025-03-14 14:54:32 +0100

    Python Program Uppgift

---
+++ uppgift2.py
@@ -0,0 +1,14 @@
+# Fråga efter användarens föddelse år
+# Beräkna vilket år de fyller 100
+# Fråga efter användarens ålder
+# Fråga efter användarens namn 
+# Visa resultatet
+
+
+a = int(input("vilket år är du född? "))
+b = a + 100
+c = int(input("hur gammal är du? "))
+d = input("vad heter du?")
+print ("hej",d ,"du är",c , "år gammal, och fyller hundra år", b)
+
+
