79837adb474cb42bb5a8a029506c54366d05cf82
minya paulsson <minya.paulsson@hotmail.com>
2025-03-22 15:39:10 +0100

    lade till uppgift.py

---
+++ uppgift.py
@@ -0,0 +1,11 @@
+
+namn = input ("Vad heter du? ")
+alder = int(input("Hur gammal är du? "))
+
+current_year = 2025
+year_100 = current_year - alder + 100
+print(f"Hej {namn}, du kommer att fylla 100 år, år {year_100}.")
+
+input("Tryck på Enter för att avsluta...")
+
+
